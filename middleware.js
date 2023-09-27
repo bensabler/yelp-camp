0fa541be0abd05debf19e64819effbfba7bf5b54
Ben Sabler <bensabler@protonmail.com>
2023-09-27 16:19:57 -0400

    improve user experience by creating middleware that redirects user to previously attempted page access

--- middleware.js
+++ middleware.js
@@ -1,9 +1,16 @@
 module.exports.isLoggedIn = (req, res, next) => {
-  console.log("REQ.USER...", req.user);
   // Ensure the user is authenticated before accessing the form
   if (!req.isAuthenticated()) {
+    req.session.returnTo = req.originalUrl; // Store the url the user is requesting
     req.flash("error", "You must be signed in first!");
     return res.redirect("/login");
+  }
+  next();
+};
+
+module.exports.storeReturnTo = (req, res, next) => {
+  if (req.session.returnTo) {
+    res.locals.returnTo = req.session.returnTo;
   }
   next();
 };
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const passport = require("passport");
 const catchAsync = require("../utils/catchAsync");
 const User = require("../models/user");
+const { storeReturnTo } = require("../middleware");
 
 // Route to display the registration form
 router.get("/register", (req, res) => {
@@ -28,7 +29,7 @@ router.post(
           return next(err);
         }
         // Flash a welcome message to the user
-        req.flash("success", "Welcome to Yelp Camp!");
+        req.flash("success", "Welcome to YelpCamp!");
         // Redirecting user to campgrounds page after successful registration
         res.redirect("/campgrounds");
       });
@@ -49,6 +50,9 @@ router.get("/login", (req, res) => {
 // Route to handle user login
 router.post(
   "/login",
+  // use the storeReturnTo middleware to save the returnTo value from session to res.locals
+  storeReturnTo,
+  // passport.authenticate logs the user in and clears req.session
   // Using passport's authenticate method to validate user's credentials
   passport.authenticate("local", {
     failureFlash: true, // Flash an error message if authentication fails
@@ -57,8 +61,10 @@ router.post(
   (req, res) => {
     // If authentication is successful, flash a welcome back message
     req.flash("success", "Welcome back!");
+    const redirectUrl = res.locals.returnTo || "/campgrounds";
+    delete req.session.returnTo;
     // Redirect user to campgrounds page after successful login
-    res.redirect("/campgrounds");
+    res.redirect(redirectUrl);
   }
 );
 
