40cf3e92432e60c6d18496d919895975b51a4eef
Ben Sabler <bensabler@protonmail.com>
2023-09-27 09:47:32 -0400

    add delete operation for reviews

--- app.js
+++ app.js
@@ -126,6 +126,18 @@ app.post(
   })
 );
 
+app.delete(
+  "/campgrounds/:id/reviews/:reviewId",
+  catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, {
+      $pull: { reviews: reviewId },
+    });
+    await Review.findByIdAndDelete(req.params.reviewId);
+    res.redirect(`/campgrounds/${id}`);
+  })
+);
+
 app.all("*", (req, res, next) => {
   next(new ExpressError("Page Not Found", 404));
 });
