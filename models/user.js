fd7a4e966bfc17a153ba800bf72a5660a1ffd8c5
Ben Sabler <bensabler@protonmail.com>
2023-09-27 13:38:16 -0400

    create register route logic

--- models/user.js
+++ models/user.js
@@ -2,11 +2,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require("passport-local-mongoose");
 
+// define User schema
 const UserSchema = new Schema({
-  email: { type: String, required: true, unique: true },
+  email: {
+    type: String, //  type is a string
+    required: true, // email is required
+    unique: true, // email must be unique
+  },
 });
 
-// passport-local-mongoose adds username and password fields to our schema
+// passport-local-mongoose adds username and password fields to the schema
 UserSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", UserSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const catchAsync = require("../utils/catchAsync");
 const User = require("../models/user");
 
+// show the register form
 router.get("/register", (req, res) => {
-  res.render("users/register");
+  res.render("users/register"); // render the register form
 });
 
-router.post("/register", async (req, res) => {
-  res.send(req.body);
-});
+// register a new user
+router.post(
+  "/register",
+  // wrapper that allows us to catch errors
+  catchAsync(async (req, res) => {
+    try {
+      const { email, username, password } = req.body; // destructure from req.body
+      const user = new User({ email, username }); // create a new user
+      const registeredUser = await User.register(user, password); // register the user with passport
+      req.flash("success", "Welcome to Yelp Camp!"); // flash a success message
+      res.redirect("/campgrounds"); // redirect to /campgrounds
+    } catch (err) {
+      req.flash("error", err.message); // flash an error message
+      res.redirect("/register"); // redirect to /register
+    }
+  })
+);
 
 module.exports = router;
