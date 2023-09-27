1fe4dcbc4d5a87249b75751e9f70804f2fdfc240
Ben Sabler <bensabler@protonmail.com>
2023-09-27 15:01:41 -0400

    implement logout operation, restyled header

--- routes/users.js
+++ routes/users.js
@@ -55,4 +55,15 @@ router.post(
   }
 );
 
+// Route to handle user logout
+router.get("/logout", (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    req.flash("success", "Goodbye!");
+    res.redirect("/campgrounds");
+  });
+}); 
+
 module.exports = router;
