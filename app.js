11b821306e03e71f769f455ac0df8628b26e5f09
Ben Sabler <bensabler@protonmail.com>
2023-09-27 13:24:14 -0400

    create user register page

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user");
 
-const campgrounds = require("./routes/campgrounds");
-const reviews = require("./routes/reviews");
+const userRoutes = require("./routes/users");
+const campgroundRoutes = require("./routes/campgrounds");
+const reviewRoutes = require("./routes/reviews");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useUnifiedTopology: true,
@@ -58,17 +59,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/fakeUser", async (req, res) => {
-  const user = new User({
-    email: "bensabler@protonmail.com",
-    username: "bensabler",
-  });
-  const newUser = await User.register(user, "dummyPassword");
-  res.send(newUser);
-});
-
-app.use("/campgrounds", campgrounds);
-app.use("/campgrounds/:id/reviews", reviews);
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
 
 app.get("/", (req, res) => {
   res.render("home");
