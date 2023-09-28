bd24e39735e0782f5b4fcc42a15cc8ea4a083acd
Ben Sabler <bensabler@protonmail.com>
2023-09-27 21:21:05 -0400

    refactored routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,25 +4,33 @@ const campgrounds = require("../controllers/campground");
 const catchAsync = require("../utils/catchAsync");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
 
-// Route to display all campgrounds
-router.get("/", catchAsync(campgrounds.index));
+router
+  .route("/")
+  // Route to display all campgrounds
+  .get(catchAsync(campgrounds.index))
+  // Route to handle the creation of a new campground
+  .post(
+    isLoggedIn,
+    validateCampground,
+    catchAsync(campgrounds.createCampground)
+  );
 
 // Route to display the form for creating a new campground
 router.get("/new", isLoggedIn, campgrounds.renderCampgroundForm);
 
-// Route to handle the creation of a new campground
-router.post(
-  "/",
-  // Ensure the user is authenticated before access
-  isLoggedIn,
-  // Validate the campground data before creation
-  validateCampground,
-  // Create the new campground
-  catchAsync(campgrounds.createCampground)
-);
-
-// Route to display the details of a specific campground by its ID
-router.get("/:id", catchAsync(campgrounds.showCampground));
+router
+  .route("/:id")
+  // Route to display the details of a specific campground by its ID
+  .get(catchAsync(campgrounds.showCampground))
+  // Route to handle the updating of a specific campground
+  .put(
+    isLoggedIn,
+    isAuthor,
+    validateCampground,
+    catchAsync(campgrounds.updateCampground)
+  )
+  // Route to handle the deletion of a specific campground
+  .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 // Route to display the edit form for a specific campground
 router.get(
@@ -32,27 +40,6 @@ router.get(
   // Ensure the user is the author of the campground before access
   isAuthor,
   catchAsync(campgrounds.renderEditForm)
-);
-
-// Route to handle the updating of a specific campground
-router.put(
-  "/:id",
-  // Ensure the user is authenticated before access
-  isLoggedIn,
-  // Ensure the user is the author of the campground before access
-  isAuthor,
-  validateCampground,
-  catchAsync(campgrounds.updateCampground)
-);
-
-// Route to handle the deletion of a specific campground
-router.delete(
-  "/:id",
-  // Ensure the user is authenticated before access
-  isLoggedIn,
-  // Ensure the user is the author of the campground before access
-  isAuthor,
-  catchAsync(campgrounds.deleteCampground)
 );
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -4,30 +4,26 @@ const passport = require("passport");
 const catchAsync = require("../utils/catchAsync");
 const { storeReturnTo } = require("../middleware");
 const users = require("../controllers/user");
-const user = require("../models/user");
 
-// Route to display the registration form
-router.get("/register", users.renderRegisterForm);
+router
+  .route("/register")
+  .get(users.renderRegisterForm)
+  .post(catchAsync(users.registerUser));
 
-// Route to register a new user
-router.post("/register", catchAsync(users.registerUser));
-
-// Route to display the login form
-router.get("/login", users.renderLoginForm);
-
-// Route to handle user login
-router.post(
-  "/login",
-  // use the storeReturnTo middleware to save the returnTo value from session to res.locals
-  storeReturnTo,
-  // passport.authenticate logs the user in and clears req.session
-  // Using passport's authenticate method to validate user's credentials
-  passport.authenticate("local", {
-    failureFlash: true, // Flash an error message if authentication fails
-    failureRedirect: "/login", // Redirect back to login page if authentication fails
-  }),
-  users.loginUser
-);
+router
+  .route("/login")
+  .get(users.renderLoginForm)
+  .post(
+    // use the storeReturnTo middleware to save the returnTo value from session to res.locals
+    storeReturnTo,
+    // passport.authenticate logs the user in and clears req.session
+    // Using passport's authenticate method to validate user's credentials
+    passport.authenticate("local", {
+      failureFlash: true, // Flash an error message if authentication fails
+      failureRedirect: "/login", // Redirect back to login page if authentication fails
+    }),
+    users.loginUser
+  );
 
 // Route to handle user logout
 router.get("/logout", users.logoutUser);
