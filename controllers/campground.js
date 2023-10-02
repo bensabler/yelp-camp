2de167a03ce0fecb98154093663370e16aebff5d
Ben Sabler <bensabler@protonmail.com>
2023-10-02 12:01:07 -0400

    update campground controller and route to accomadate changes to campground edit view

--- controllers/campground.js
+++ controllers/campground.js
@@ -71,6 +71,13 @@ module.exports.updateCampground = async (req, res) => {
   const campground = await Campground.findByIdAndUpdate(id, {
     ...req.body.campground,
   });
+  const imgs = req.files.map((f) => ({
+    url: f.path,
+    filename: f.filename,
+  }));
+  campground.image.push(...imgs);
+  // Save the updated campground to the database
+  await campground.save();
   req.flash("success", "Successfully updated campground!");
   res.redirect(`/campgrounds/${campground._id}`);
 };
