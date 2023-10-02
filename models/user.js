74e47916bc60c78043da43688f6fb0207e91c66d
Ben Sabler <bensabler@protonmail.com>
2023-10-02 08:43:07 -0400

    update user model

--- models/user.js
+++ models/user.js
@@ -4,11 +4,52 @@ const passportLocalMongoose = require("passport-local-mongoose");
 
 // define User schema
 const UserSchema = new Schema({
-  email: {
-    type: String, //  type is a string
-    required: true, // email is required
-    unique: true, // email must be unique
+  name: {
+    type: String,
+    required: true,
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  tosAccepted: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  bio: {
+    type: String,
+    required: false,
+    default: "Tell us about yourself!",
+  },
+  campgrounds: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Campground",
+    },
+  ],
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+});
+
+UserSchema.pre("remove", async function (next) {
+  try {
+    // Remove all campgrounds associated with the user
+    await mongoose.model("Campground").deleteMany({ author: this._id });
+    next();
+  } catch (err) {
+    next(err);
+  }
 });
 
 // passport-local-mongoose adds username and password fields to the schema
