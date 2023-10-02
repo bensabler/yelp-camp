3e00f407ac1f723e8bf5dbd5a736b9cc3b1dc29b
Ben Sabler <bensabler@protonmail.com>
2023-10-02 08:46:59 -0400

    update form validation

--- public/javascripts/validateForms.js
+++ public/javascripts/validateForms.js
@@ -7,6 +7,19 @@
     form.addEventListener(
       "submit",
       (event) => {
+        const password = form.querySelector("#password").value;
+        const repeatPassword = form.querySelector("#form3Example4cd").value;
+
+        if (password !== repeatPassword) {
+          // If passwords don't match, set a custom validation error
+          const repeatPasswordInput = form.querySelector("#form3Example4cd");
+          repeatPasswordInput.setCustomValidity("Passwords do not match.");
+        } else {
+          // If passwords match, reset any custom validation from previous checks
+          const repeatPasswordInput = form.querySelector("#form3Example4cd");
+          repeatPasswordInput.setCustomValidity("");
+        }
+
         if (!form.checkValidity()) {
           event.preventDefault();
           event.stopPropagation();
