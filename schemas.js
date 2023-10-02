39e7017789fe30137939b1e72aebad619faa77ab
Ben Sabler <bensabler@protonmail.com>
2023-10-02 08:46:36 -0400

    create user schema

--- schemas.js
+++ schemas.js
@@ -3,7 +3,7 @@ module.exports.campgroundSchema = Joi.object({
   campground: Joi.object({
     title: Joi.string().required(),
     price: Joi.number().required().min(0),
-    image: Joi.string().required(),
+    // image: Joi.string().required(),
     location: Joi.string().required(),
     description: Joi.string().required(),
   }).required(),
@@ -15,3 +15,20 @@ module.exports.reviewSchema = Joi.object({
     body: Joi.string().required(),
   }).required(),
 });
+
+const UserJoiSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  username: Joi.string().alphanum().min(3).max(30).required(),
+  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")), // This assumes a basic alphanumeric password validation. Adjust as per your requirements.
+  repeat_password: Joi.ref("password"), // Ensure password and repeat_password are the same
+  tosAccepted: Joi.boolean().required(),
+  bio: Joi.string().allow("").default("Tell us about yourself!"), // Allows an empty string or a provided bio
+  // Since campgrounds and reviews are ObjectIds, we're assuming they'll be provided as strings in input. Adjust if necessary.
+  campgrounds: Joi.array()
+    .items(Joi.string().pattern(/^[0-9a-fA-F]{24}$/))
+    .optional(),
+  reviews: Joi.array()
+    .items(Joi.string().pattern(/^[0-9a-fA-F]{24}$/))
+    .optional(),
+}).with("password", "repeat_password"); // Ensure both password and repeat_password are present together
