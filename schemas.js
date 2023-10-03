240974da9aa5506cc712b43d64e658d12e85b2ac
Ben Sabler <bensabler@protonmail.com>
2023-10-03 10:23:19 -0400

    implement protections against xss

--- schemas.js
+++ schemas.js
@@ -1,11 +1,34 @@
-const Joi = require("joi");
+const BaseJoi = require("joi");
+const sanitizeHtml = require("sanitize-html");
+
+const extension = (joi) => ({
+  type: "string",
+  base: joi.string(),
+  messages: {
+    "string.escapeHTML": "{{#label}} must not include HTML!",
+  },
+  rules: {
+    escapeHTML: {
+      validate(value, helpers) {
+        const clean = sanitizeHtml(value, {
+          allowedTags: [],
+          allowedAttributes: {},
+        });
+        if (clean !== value)
+          return helpers.error("string.escapeHTML", { value });
+      },
+    },
+  },
+});
+
+const Joi = BaseJoi.extend(extension);
 
 module.exports.campgroundSchema = Joi.object({
   campground: Joi.object({
-    title: Joi.string().required().max(100),
+    title: Joi.string().required().max(100).escapeHTML(),
     price: Joi.number().min(0).required(),
-    location: Joi.string().required().max(200),
-    description: Joi.string().required().max(1000),
+    location: Joi.string().required().max(200).escapeHTML(),
+    description: Joi.string().required().max(1000).escapeHTML(),
     images: Joi.array()
       .items(
         Joi.object({
@@ -22,18 +45,25 @@ module.exports.campgroundSchema = Joi.object({
 module.exports.reviewSchema = Joi.object({
   review: Joi.object({
     rating: Joi.number().required().min(1).max(5),
-    body: Joi.string().required().max(500),
+    body: Joi.string().required().max(500).escapeHTML(),
   }).required(),
 });
 
 module.exports.userSchema = Joi.object({
-  name: Joi.string().required().max(100),
-  email: Joi.string().email().required().max(200),
-  username: Joi.string().alphanum().min(3).max(30).required(),
-  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).max(30),
+  name: Joi.string().required().max(100).escapeHTML(),
+  email: Joi.string().email().required().max(200).escapeHTML(),
+  username: Joi.string().alphanum().min(3).max(30).required().escapeHTML(),
+  password: Joi.string()
+    .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+    .max(30)
+    .escapeHTML(),
   repeat_password: Joi.ref("password"),
   tosAccepted: Joi.boolean().required(),
-  bio: Joi.string().allow("").default("Tell us about yourself!").max(500),
+  bio: Joi.string()
+    .allow("")
+    .default("Tell us about yourself!")
+    .max(500)
+    .escapeHTML(),
   campgrounds: Joi.array()
     .items(Joi.string().pattern(/^[0-9a-fA-F]{24}$/))
     .optional(),
